bf93f86ca34731e7dec4c0b4a3e4f6a9c35cb77d
MarynaOzkara <ozkaramaryna@gmail.com>
2023-09-26 04:39:22 +0300

    final hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ const App =() => {
   const [totalPages, setTotalPages] = useState(0);
   const [currrentImage, setCurrentImage] = useState(null);
   const [currentTags, setCurrentTags] = useState(null);
-  const [error, setError] = useState(null);
+  
 
  const createSearchQuery = (searchQuery) =>{
         // console.log(searchQuery);
@@ -60,7 +60,7 @@ useEffect(() =>{
       //  console.log(hits);
       //  console.log(totalHits);
     } catch (error) {
-         setError(error.message);
+         console.log(error.message);
          alert ('Sorry, something went wrong! Try again later!')
     } finally {
       setIsLoading(false);
@@ -86,112 +86,7 @@ useEffect(() =>{
   )
 }
 export default App;
-// export class App extends Component {
-//   state = {
-//       query: '',
-//       page: 0,
-//       images: null,
-//       totalImages: 0,
-//       totalPages: 0,
-//       showModal: false,
-//       error: false,
-//       isLoading: false,
-//       currrentImage: null,
-//       currentTags: null,
-      
-//   }
 
-//   componentDidUpdate(prevProps, prevState){
-//     const {query, page} = this.state;
-
-//     if(prevState.query !== query) {
-      
-//       getImages(query)
-//       .then(({hits, totalHits}) => {
-//         if(!totalHits){
-//           alert ('We didn find any matches! Try to type another query')
-//           return;
-//         }
-//         this.setState(({isLoading}) => ({isLoading: !isLoading}));
-//         const imagesArray = hits.map(hit => ({
-//           id: hit.id,
-//           webformatURL: hit.webformatURL,
-//           tags: hit.tags,
-//           largeImageURL: hit.largeImageURL,
-//         }));
-//         this.setState({
-//           page: 1,
-//           images: imagesArray,
-//           totalImages: totalHits,
-//           totalPages: Math.ceil(totalHits / 12), 
-//         })
-        
-//       })   
-//       .catch(error => this.setState({ error }))  
-//       .finally(() => this.setState(({isLoading}) => ({isLoading: !isLoading})))
-//     };
-   
-//     if(prevState.page !== page && page !== 1) {
-
-//       this.setState(({isLoading}) => ({isLoading: !isLoading}));
-
-//       getImages(query, page)
-//       .then(({hits}) => {
-       
-//         const imagesArray = hits.map(hit => ({
-//           id: hit.id,
-//           webformatURL: hit.webformatURL,
-//           tags: hit.tags,
-//           largeImageURL: hit.largeImageURL,
-//         }));
-//         this.setState(({images}) => ({
-//           images: [...images, ...imagesArray],
-//         }))
-//       })
-//       .catch(error => this.setState({ error }))
-//       .finally(() => this.setState(({isLoading}) => ({isLoading: !isLoading})))
-//     }
-//   }
-
-// createSearchQuery = (searchQuery) =>{
-//     console.log(searchQuery);
-//     this.setState({
-//       query: searchQuery, 
-//     })
-//   }
-// loadMoreImages = () => {
-//     this.setState(({page}) => ({page: page + 1}))
-//   }
-// toggleModal = () => {
-//     this.setState(({ showModal }) => ({ showModal: !showModal }));
-//   };
-// openModal = (largeImageURL, tags) => {
-   
-//     this.setState(({showModal}) => ({
-//       showModal: !showModal,
-//       currrentImage: largeImageURL,
-//       currentTags: tags,
-//     }))
-//   }
-// render() {
-//   const {images, showModal, isLoading, currrentImage, currentTags, totalPages, page} = this.state;
-//  console.log(totalPages);
-//   return (
-//     <>
-//     <Searchbar createSearchQuery={this.createSearchQuery}/>
-//     {images && <ImageGallery images={images} openModal={this.openModal}/>}
-//     {isLoading && <Loader/>}
-//     {page > 0 && totalPages !== page && <Button loadMoreImages={this.loadMoreImages}/>}
-     
-     
-//      {showModal && <Modal onClose={this.toggleModal} image={currrentImage} tags={currentTags}/>}
-    
-     
-//   </>
-//   )
-
-// }
-// }
  
 
  
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,42 +1,70 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
+const Modal = ({image, tags, onClose}) => {
+  useEffect(() =>{
+  const handleKeyDown = e => {
+              if (e.code === 'Escape') {
+                onClose();
+              }
+            };
+   window.addEventListener('keydown', handleKeyDown);
+   return () => {
+    window.removeEventListener('keydown', handleKeyDown); 
+   }          
+  }, [onClose])
 
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-      }
+ const handleClickBackdrop = e => {
+            if (e.target === e.currentTarget) {
+              onClose();
+            }
+    };
+
+  return createPortal(
+    <div className={css.backDrope} onClick={handleClickBackdrop}>
+       <div className={css.modal}>
+         <img src={image} alt={tags} />
+       </div>
+    </div>, 
+    modalRoot
+ );
+}
+// export class Modal extends Component {
+
+//     componentDidMount() {
+//         window.addEventListener('keydown', this.handleKeyDown);
+//       }
     
-      componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-      }
+//       componentWillUnmount() {
+//         window.removeEventListener('keydown', this.handleKeyDown);
+//       }
       
-      handleKeyDown = e => {
-        if (e.code === 'Escape') {
-          this.props.onClose();
-        }
-      };
+//       handleKeyDown = e => {
+//         if (e.code === 'Escape') {
+//           this.props.onClose();
+//         }
+//       };
 
-      handleClickBackdrop = e => {
-        if (e.target === e.currentTarget) {
-          this.props.onClose();
-        }
-      };
+//       handleClickBackdrop = e => {
+//         if (e.target === e.currentTarget) {
+//           this.props.onClose();
+//         }
+//       };
     
           
- render(){
-    const {image, tags} = this.props;
-    return createPortal(
-       <div className={css.backDrope} onClick={this.handleClickBackdrop}>
-          <div className={css.modal}>
-            <img src={image} alt={tags} />
-          </div>
-       </div>, 
-       modalRoot
-    );
- }
-}
+//  render(){
+//     const {image, tags} = this.props;
+//     return createPortal(
+//        <div className={css.backDrope} onClick={this.handleClickBackdrop}>
+//           <div className={css.modal}>
+//             <img src={image} alt={tags} />
+//           </div>
+//        </div>, 
+//        modalRoot
+//     );
+//  }
+// }
  export default Modal;
